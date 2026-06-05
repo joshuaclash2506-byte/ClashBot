45006c0e52b371898fbe9015da6e994deaf2c3ef
Josh2506 <joshuaclash2506@gmail.com>
2026-06-05 18:52:28 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Spielt Clas of Clans",
+        name: "Spielt Clash of Clans",
         // Activity type number (0 = Playing).
         type: 0, 
       },
